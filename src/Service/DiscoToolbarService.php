0210489d5e7df1bcad82a43e0796282596975a3d
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2026-06-19 19:01:35 +0200

    Fixed issue with `InjectDiscoToolbar` middleware when handling `JsonResponse` data.

--- src/Service/DiscoToolbarService.php
+++ src/Service/DiscoToolbarService.php
@@ -76,7 +76,7 @@ class DiscoToolbarService
         if (\is_array($fontAwesomeConfig)) {
             $fontAwesomeEnabled = $fontAwesomeConfig['enabled'] ?? false;
             $userVersion = $fontAwesomeConfig['version'] ?? null;
-            if ($userVersion !== null && \is_string($userVersion)) {
+            if (\is_string($userVersion)) {
                 $fontAwesomeVersion = $userVersion;
             }
         }
@@ -159,7 +159,7 @@ class DiscoToolbarService
         try {
             $version = InstalledVersions::getVersion('marcin-orlowski/disco-toolbar-laravel');
             return $version ?? self::DEV_VERSION ;
-        } catch (\Exception $e) {
+        } catch (\Throwable) {
             return self::DEV_VERSION;
         }
     }
